00cc227d6a92f9f6a8d99dce24897e23e8f1aa2c
Sujon Mahmud <sujonmahmud@Sujons-MacBook-Air.local>
2021-10-06 09:24:04 +0600

    file upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
 import Logo from '../assets/img/logo.png';
-
+import LogoBNB from '../assets/img/bnb.png';
 const Header = () => {
      return (
-          <div className="header-area">
-               <div className="logo">
-                    <Link><img src={Logo} />Deliverer Defi File Sharing</Link>
-               </div>
-               <div className="menu-list"><Link to="#">...</Link></div>
-
+          <>  
+          <div className="top-logo">
+               <Link to="#"><img src={LogoBNB} /></Link>
           </div>
+               <div className="header-area">
+                    <div className="logo">
+                         <Link><img src={Logo} />Deliverer Defi File Sharing</Link>
+                    </div>
+                    <div className="menu-list"><Link to="#">...</Link></div>
+
+               </div>
+          </>
      )
 }
 
